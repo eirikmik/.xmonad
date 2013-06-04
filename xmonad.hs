625721b0f28022344760a618f1b903deabc2fad2
Eirik Mikkelsen <eirikmik@gmail.com>
2013-06-04 18:18:05 +0200

    Initial commit of my Xmonad configuration

---
+++ xmonad.hs
@@ -0,0 +1,32 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Gaps
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+ 
+
+myManageHook :: [ManageHook]
+myManageHook = 
+	[ resource =? "Do" --> doIgnore ]
+
+
+main = do
+  xmproc <- spawnPipe "/usr/bin/xmobar /home/eirikmik/.xmonad/xmobarrc"
+  synapse <- spawnPipe "/usr/bin/synapse -s"
+  trayer <- spawnPipe "trayer --transparent true --tint FFFFFF --alpha 0 --edge top --align right --widthtype pixel --width 250 --heighttype pixel --height 17"
+  gnomesession <- spawnPipe "gnome-session"
+  xmonad defaultConfig {
+    modMask = mod4Mask, 
+    terminal = "gnome-terminal",
+    manageHook = ( isFullscreen --> doFullFloat ) <+> manageDocks <+> manageHook defaultConfig <+> composeAll myManageHook,
+--    manageHook = manageDocks <+> manageHook defaultConfig,
+    layoutHook = avoidStruts $ layoutHook defaultConfig, 
+    logHook = dynamicLogWithPP $ xmobarPP
+                        { ppOutput = hPutStrLn xmproc,
+                          ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+  }
